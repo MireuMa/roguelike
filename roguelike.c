fa6cf89e8d366b1373a91e9c485a062214591fe1
MireuMa <mireubay1@gmail.com>
2024-05-26 22:46:48 +0900

    Update roguelike.c

--- roguelike.c
+++ roguelike.c
@@ -12,7 +12,7 @@
 #define TOXIC_GAS_DURATION 5
 
 typedef struct {
-    char** map;
+    char* map;
     int HEIGHT;
     int WIDTH;
 } Map;
@@ -35,169 +35,148 @@ void resetToxicGasTimerAndRestoreHealth(int* health, int* toxicGasTimer);
 void initializeMaps();
 void freeMaps();
 
-// 상수 맵 정의
-const char* map1[] = {
-    "WWWGWWWWWWW",
-    "S     T  CS",
-    "S & C     S",
-    "SWWWWWW+  S",
-    "S   & C  +S",
-    "S    WWWWWS",
-    "S  T    C S",
-    "WWWWWWWWEWW"
-};
-
-const char* map2[] = {
-    "WWWWWWWW",
-    "S  TC  G",
-    "S &  WWS",
-    "SWW+ T S",
-    "E SC   S",
-    "S S &  S",
-    "SC   C S",
-    "WWWWWWWW"
-};
-
-const char* map3[] = {
-    "WWWGWWW",
-    "S+  T S",
-    "S  C  S",
-    "ST WWWS",
-    "S & +CS",
-    "S T   S",
-    "SWWWW+S",
-    "S  C  S",
-    "WWWEWWW"
-};
-
-const char* map4[] = {
-    "WWWWWWWWWW",
-    "S  C  S+ S",
-    "S  S  ST E",
-    "S  S TC  S",
-    "S CS  S& S",
-    "S  S& S TS",
-    "ST  +    S",
-    "WWWWWGWWWW"
-};
-
-const char* map5[] = {
-    "WWWWWWWWWW",
-    "S   T    G",
-    "S+   S C S",
-    "S   &WWWWS",
-    "S  C T   S",
-    "S &   +  S",
-    "S WWWWWW S",
-    "S TC  +  S",
-    "S      C S",
-    "WWEWWWWWWW"
-};
-
-const char* map6[] = {
-    "WWWWWWWWWWW",
-    "S +    C  S",
-    "S  TT     S",
-    "S  + S &  S",
-    "SWWW S  T S",
-    "S &  S    E",
-    "S   WWWWWWS",
-    "S T   +  CS",
-    "S    C    S",
+const char* map1 =
     "WWWGWWWWWWW"
-};
+    "S     T  CS"
+    "S & C     S"
+    "SWWWWWW+  S"
+    "S   & C  +S"
+    "S    WWWWWS"
+    "S  T    C S"
+    "WWWWWWWWEWW";
 
-const char* map7[] = {
-    "WWGWWWW",
-    "S  C  S",
-    "SWW+T S",
-    "E S+ &S",
-    "S S T S",
-    "S+SW  S",
-    "S & C+S",
-    "WWWWWWW"
-};
+const char* map2 =
+    "WWWWWWWW"
+    "S  TC  G"
+    "S &  WWS"
+    "SWW+ T S"
+    "E SC   S"
+    "S S &  S"
+    "SC   C S"
+    "WWWWWWWW";
 
-const char* map8[] = {
-    "WWWWWWWWWW",
-    "S C& +   S",
-    "S   S   CS",
-    "ST TS  T S",
-    "E   S &  S",
-    "S  CS C  S",
-    "WWWWWWWGWW"
-};
+const char* map3 =
+    "WWWGWWW"
+    "S+  T S"
+    "S  C  S"
+    "ST WWWS"
+    "S & +CS"
+    "S T   S"
+    "SWWWW+S"
+    "S  C  S"
+    "WWWEWWW";
 
-const char* map9[] = {
-    "WWWWWWW",
-    "S C   S",
-    "S  &+ S",
-    "S     E",
-    "S& WWWS",
-    "S  C  S",
-    "SWWWW S",
-    "G & T S",
-    "S +   S",
-    "S  C  S",
-    "WWWWWWW"
-};
-
-const char* map10[] = {
-    "WWWEWWWWWW",
-    "S C  &  CS",
-    "SW TWWW +S",
-    "S   &  & S",
-    "SC+  SWWWS",
-    "S   TS   G",
-    "S  C ST  S",
-    "S TC +   S",
+const char* map4 =
     "WWWWWWWWWW"
-};
+    "S  C  S+ S"
+    "S  S  ST E"
+    "S  S TC  S"
+    "S CS  S& S"
+    "S  S& S TS"
+    "ST  +    S"
+    "WWWWWGWWWW";
+
+const char* map5 =
+    "WWWWWWWWWW"
+    "S   T    G"
+    "S+   S C S"
+    "S   &WWWWS"
+    "S  C T   S"
+    "S &   +  S"
+    "S WWWWWW S"
+    "S TC  +  S"
+    "S      C S"
+    "WWEWWWWWWW";
+
+const char* map6 =
+    "WWWWWWWWWWW"
+    "S +    C  S"
+    "S  TT     S"
+    "S  + S &  S"
+    "SWWW S  T S"
+    "S &  S    E"
+    "S   WWWWWWS"
+    "S T   +  CS"
+    "S    C    S"
+    "WWWGWWWWWWW";
+
+const char* map7 =
+    "WWGWWWW"
+    "S  C  S"
+    "SWW+T S"
+    "E S+ &S"
+    "S S T S"
+    "S+SW  S"
+    "S & C+S"
+    "WWWWWWW";
+
+const char* map8 =
+    "WWWWWWWWWW"
+    "S C& +   S"
+    "S   S   CS"
+    "ST TS  T S"
+    "E   S &  S"
+    "S  CS C  S"
+    "WWWWWWWGWW";
+
+const char* map9 =
+    "WWWWWWW"
+    "S C   S"
+    "S  &+ S"
+    "S     E"
+    "S& WWWS"
+    "S  C  S"
+    "SWWWW S"
+    "G & T S"
+    "S +   S"
+    "S  C  S"
+    "WWWWWWW";
+
+const char* map10 =
+    "WWWEWWWWWW"
+    "S C  &  CS"
+    "SW TWWW +S"
+    "S   &  & S"
+    "SC+  SWWWS"
+    "S   TS   G"
+    "S  C ST  S"
+    "S TC +   S"
+    "WWWWWWWWWW";
 
 Map maps[NUM_MAPS];
 
-// 동적 메모리 할당으로 맵 초기화 함수
 void initializeMaps() {
-    const char** staticMaps[NUM_MAPS] = { map1, map2, map3, map4, map5, map6, map7, map8, map9, map10 };
-    int heights[NUM_MAPS] = { 8, 8, 9, 8, 10, 10, 8, 7, 11, 9 };
-    int widths[NUM_MAPS] = { 11, 8, 7, 10, 10, 11, 7, 10, 8, 10 };
+    const char* staticMaps[NUM_MAPS] = {map1, map2, map3, map4, map5, map6, map7, map8, map9, map10};
+    int heights[NUM_MAPS] = {8, 8, 9, 8, 10, 10, 8, 7, 11, 9};
+    int widths[NUM_MAPS] = {11, 8, 7, 10, 10, 11, 7, 10, 8, 10};
 
-    // 각 맵에 대해 동적 메모리 할당 및 초기화
     for (int i = 0; i < NUM_MAPS; i++) {
         maps[i].HEIGHT = heights[i];
         maps[i].WIDTH = widths[i];
-        maps[i].map = (char**)malloc(maps[i].HEIGHT * sizeof(char*));
-        for (int j = 0; j < maps[i].HEIGHT; j++) {
-            maps[i].map[j] = (char*)malloc((maps[i].WIDTH + 1) * sizeof(char)); // +1은 NULL terminator때문
-            strcpy(maps[i].map[j], staticMaps[i][j]);
-        }
+        maps[i].map = (char*)malloc((heights[i] * widths[i] + 1) * sizeof(char)); // +1 for null terminator
+        strcpy(maps[i].map, staticMaps[i]);
     }
 }
 
-// 동적으로 할당된 맵 메모리 해제 함수
 void freeMaps() {
     for (int i = 0; i < NUM_MAPS; i++) {
-        for (int j = 0; j < maps[i].HEIGHT; j++) {
-            free(maps[i].map[j]);
-        }
         free(maps[i].map);
     }
 }
 
-//김현수 신호연
 int main() {
     srand(time(NULL));
 
     hideCursor(); // 커서 숨김
     gameStartScreen(); // 게임 시작 화면을 출력
 
-    initializeMaps(); // 맵을 초기화
+    initializeMaps(); // 맵 초기화
 
     int gameWon = 1; // 게임 승리 여부를 나타내는 변수
     int totalCoins = 0; // 총 획득한 동전 수
 
     // 맵 중복 선택 방지를 위한 배열
-    int usedMaps[NUM_MAPS] = { 0 };
+    int usedMaps[NUM_MAPS] = {0};
 
     for (int stage = 0; stage < STAGES; ++stage) {
         int mapIndex;
@@ -213,8 +192,7 @@ int main() {
             gameWon = 0;
             totalCoins += (-coins);
             break; // 플레이어가 죽었으므로 게임 종료
-        }
-        else {
+        } else {
             totalCoins += coins;
         }
         printf("다음 단계로 진행 중...\n");
@@ -225,62 +203,61 @@ int main() {
         gameCompleteScreen(totalCoins); // 게임이 완료되었음을 알리는 화면 출력
     }
 
-    freeMaps(); // 맵 메모리 해제
+    freeMaps(); // 동적 메모리 해제
 
     return 0;
 }
 
-//마미르 박하늘
+// 플레이어의 이동 처리
 void handleMovement(Map* map, int* x, int* y, char move) {
     int newX = *x;
     int newY = *y;
 
     switch (move) {
-    case 'w': newY--; break; // 위로 이동
-    case 's': newY++; break; // 아래로 이동
-    case 'a': newX--; break; // 왼쪽으로 이동
-    case 'd': newX++; break; // 오른쪽으로 이동
-    default: break;
+        case 'w': newY--; break; // 위로 이동
+        case 's': newY++; break; // 아래로 이동
+        case 'a': newX--; break; // 왼쪽으로 이동
+        case 'd': newX++; break; // 오른쪽으로 이동
+        default: break;
     }
 
-    // S 충돌 추가
-    if (newX >= 0 && newX < map->WIDTH && newY >= 0 && newY < map->HEIGHT && map->map[newY][newX] != 'W' && map->map[newY][newX] != 'S') {
+    // 맵 경계와 충돌 체크
+    if (newX >= 0 && newX < map->WIDTH && newY >= 0 && newY < map->HEIGHT && map->map[newY * map->WIDTH + newX] != 'W' && map->map[newY * map->WIDTH + newX] != 'S') {
         *x = newX;
         *y = newY;
     }
 }
 
-//마미르
-// 수정(매개변수 추가)
+// 셀 체크 및 효과 적용
 void checkCell(Map* map, int x, int y, int* health, int* coins, int* toxicGasTimer, DWORD* lastUpdate, DWORD* messageLastUpdate, DWORD* currentTime, char* statusMessage) {
-    switch (map->map[y][x]) {
-    case 'T':
-        toxicGasEffect(toxicGasTimer); // 유독 가스 효과 적용
-        snprintf(statusMessage, 256, "유독 가스를 만났습니다!");
-        if (*toxicGasTimer == 0) {
-            *lastUpdate = *currentTime;
-        }
-        *messageLastUpdate = *currentTime;
-        break;
-    case 'C':
-        snprintf(statusMessage, 256, "동전을 획득했습니다!");
-        (*coins)++;
-        map->map[y][x] = ' ';
-        *messageLastUpdate = *currentTime;
-        break;
-    case '&':
-        snprintf(statusMessage, 256, "적을 만났습니다!");
-        (*health)--;
-        *messageLastUpdate = *currentTime;
-        break;
-    case '+':
-        resetToxicGasTimerAndRestoreHealth(health, toxicGasTimer); // 체력 회복 및 유독 가스 제거
-        snprintf(statusMessage, 256, "체력이 회복되고 유독 가스가 제거되었습니다!");
-        map->map[y][x] = ' ';
-        *messageLastUpdate = *currentTime;
-        break;
-    default:
-        break;
+    switch (map->map[y * map->WIDTH + x]) {
+        case 'T':
+            toxicGasEffect(toxicGasTimer); // 유독 가스 효과 적용
+            snprintf(statusMessage, 256, "유독 가스를 만났습니다!");
+            if (*toxicGasTimer == 0) {
+                *lastUpdate = *currentTime;
+            }
+            *messageLastUpdate = *currentTime;
+            break;
+        case 'C':
+            snprintf(statusMessage, 256, "동전을 획득했습니다!");
+            (*coins)++;
+            map->map[y * map->WIDTH + x] = ' ';
+            *messageLastUpdate = *currentTime;
+            break;
+        case '&':
+            snprintf(statusMessage, 256, "적을 만났습니다!");
+            (*health)--;
+            *messageLastUpdate = *currentTime;
+            break;
+        case '+':
+            resetToxicGasTimerAndRestoreHealth(health, toxicGasTimer); // 체력 회복 및 유독 가스 제거
+            snprintf(statusMessage, 256, "체력이 회복되고 유독 가스가 제거되었습니다!");
+            map->map[y * map->WIDTH + x] = ' ';
+            *messageLastUpdate = *currentTime;
+            break;
+        default:
+            break;
     }
 }
 
@@ -310,41 +287,45 @@ void clearMap() {
 }
 
 void hideCursor() {
-    CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+    CONSOLE_CURSOR_INFO cursorInfo = {0,};
     cursorInfo.bVisible = 0;
     cursorInfo.dwSize = 1;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
 void setCursorPosition(int x, int y) {
-    printf("\033[%d;%dH", y + 1, x + 1);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+// 맵 출력
 void printMap(Map* map, int playerX, int playerY) {
     for (int row = 0; row < map->HEIGHT; row++) {
         for (int column = 0; column < map->WIDTH; column++) {
             if (row == playerY && column == playerX) {
                 printf(" P"); // 플레이어 위치 출력
-            }
-            else {
+            } else {
                 // 맵 요소에 따라 다른 문자를 출력
-                if (map->map[row][column] == 'W')
+                char cell = map->map[row * map->WIDTH + column];
+                if (cell == 'W')
                     printf(" ─");
-                else if (map->map[row][column] == 'S')
+                else if (cell == 'S')
                     printf(" |");
-                else if (map->map[row][column] == ' ')
+                else if (cell == ' ')
                     printf("  "); // 공백으로 업데이트
-                else if (map->map[row][column] == 'E')
+                else if (cell == 'E')
                     printf(" E");
-                else if (map->map[row][column] == 'G')
+                else if (cell == 'G')
                     printf(" G");
-                else if (map->map[row][column] == 'T')
+                else if (cell == 'T')
                     printf(" T");
-                else if (map->map[row][column] == 'C')
+                else if (cell == 'C')
                     printf(" C");
-                else if (map->map[row][column] == '&')
+                else if (cell == '&')
                     printf(" &");
-                else if (map->map[row][column] == '+')
+                else if (cell == '+')
                     printf(" +");
             }
         }
@@ -352,6 +333,7 @@ void printMap(Map* map, int playerX, int playerY) {
     }
 }
 
+// UI 출력
 void printUI(int health, int coins, int toxicGasTimer, char* statusMessage) {
     printf("\033[1H");
     printf("체력: %d | 동전: %d | 유독 가스 타이머: %d\n", health, coins, toxicGasTimer);
@@ -359,6 +341,7 @@ void printUI(int health, int coins, int toxicGasTimer, char* statusMessage) {
     printf("%s\n", statusMessage);
 }
 
+// 게임 오버 화면 출력
 void gameOverScreen(int coins) {
     clearScreen();
     printf("========================================\n");
@@ -372,19 +355,43 @@ void gameOverScreen(int coins) {
     exit(0);
 }
 
+// 게임 완료 화면 출력
 void gameCompleteScreen(int coins) {
     clearScreen();
     printf("========================================\n");
     printf("             게임 완료             \n");
     printf("========================================\n");
     printf("축하합니다! 모든 단계를 완료했습니다.\n");
-    printf("총 획득한 동전: %d\n", coins);
+
+    setCursorPosition(17, 11); // (18, 12) in 1-based index
+    printf(" P");
+    setCursorPosition(17, 6);  // (18, 7) in 1-based index
+    printf(" A");
+
+    fflush(stdout);
+    Sleep(1000); // 1초 대기
+
+    // 반복문을 사용하여 A를 아래로 이동
+    for (int i = 6; i < 11; i++) {
+        setCursorPosition(17, i);  // 이전 위치를 지움
+        printf("  ");
+        setCursorPosition(17, i + 1);  // 새로운 위치에 A를 출력
+        printf(" A");
+        fflush(stdout);
+        Sleep(1000); // 1초 대기
+    }
+
+    setCursorPosition(0, 13); // (1, 14) in 1-based index
+    printf("당신은 족보를 얻었습니다. 시험 힘내세요!\n");
     printf("========================================\n");
     printf("나가려면 아무 키나 누르세요...\n");
-    _getch();
+
+    fflush(stdout);
+    _getch(); // 키 입력 대기
     exit(0);
 }
 
+// 게임 시작 화면 출력
 void gameStartScreen() {
     clearScreen();
     printf("=============================================================================\n");
@@ -399,6 +406,7 @@ void gameStartScreen() {
     _getch();
 }
 
+// 게임 플레이 함수
 int playGame(Map* map) {
     int x = -1, y = -1;
     int health = INITIAL_HEALTH;
@@ -410,7 +418,7 @@ int playGame(Map* map) {
     // 'G'로 표시된 시작 위치 찾기
     for (int row = 0; row < map->HEIGHT; row++) {
         for (int col = 0; col < map->WIDTH; col++) {
-            if (map->map[row][col] == 'G') {
+            if (map->map[row * map->WIDTH + col] == 'G') {
                 x = col;
                 y = row;
                 break;
@@ -435,7 +443,7 @@ int playGame(Map* map) {
         DWORD currentTime = GetTickCount();
 
         // 플레이어가 출구에 도달했는지 확인
-        if (map->map[y][x] == 'E') {
+        if (map->map[y * map->WIDTH + x] == 'E') {
             printf("\033[0m"); // 텍스트 색상 재설정
             printf("\n출구에 도착했습니다!\n");
             return coins;
